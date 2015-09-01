92b0ce2edd10a1cbd9a5e82eba7ffb1a5092ae78
steve <steve@stephenschafer.com>
2015-09-01 13:05:29 -0600

    rm bin

--- bin/scriptOutput/reportviewer/reporting-require-js-cfg.js
+++
@@ -1,8 +0,0 @@
-var prefix = (typeof CONTEXT_PATH != "undefined") ? CONTEXT_PATH+'content/reporting/reportviewer' : 'reportviewer'; 
-if(typeof document == "undefined" || document.location.href.indexOf("debug=true") > 0){
-  requireCfg['paths']['reportviewer'] = prefix;
-  requireCfg['paths']['pentaho/reportviewer'] = prefix+'/dojo/pentaho/reportviewer';
-} else {
-  requireCfg['paths']['reportviewer'] = prefix /* +'/compressed' */;
-  requireCfg['paths']['pentaho/reportviewer'] = prefix+'/dojo/pentaho/reportviewer';
-}
